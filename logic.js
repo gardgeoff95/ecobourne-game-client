3255cdd3300b289c08e60dc49ae957240b9e5071
Geoff <gardgeoff95@email.arizona.edu>
2019-10-09 20:16:34 -0700

    more d3

--- logic.js
+++ logic.js
@@ -1,7 +1,3 @@
-
-
-
-
 var firebaseConfig = {
   apiKey: "AIzaSyAaktd7xWg2F92a5py9ZBB5fdsySImFOGQ",
   authDomain: "ecobourne-fb892.firebaseapp.com",
@@ -11,10 +7,11 @@ var firebaseConfig = {
   messagingSenderId: "342132988603",
   appId: "1:342132988603:web:59feab64b679748217279e"
 };
+
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database();
 
-database.ref("/").set("")
+database.ref("/").set("");
 let board = {
   maxTiles: 100,
   tileWidth: 16,
@@ -48,6 +45,7 @@ let maxBunnies = 200;
 let timeStampNum = 0;
 let time = 0;
 let muted = false;
+let stop = false;
 let speedModifier = 1;
 let toolBox;
 let sliderVal = $(speedSlider).val() / speedModifier;
@@ -60,6 +58,8 @@ let foxImg = new Image();
 let bearImg = new Image();
 let grassImg = new Image();
 let pop = new Audio("blop.wav");
+let d3Index = 0;
+
 grassImg.src = "grassTiles.png";
 bunnyImg.src = "rabbit.png";
 foxImg.src = "fox.png";
@@ -113,8 +113,6 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
 
   //Function called every time the startGame interval is called
   this.stateManager = function() {
-   
-
     if (this.state === "idle") {
       this.move();
     } else if (
@@ -135,10 +133,7 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
     ) {
       this.preyBunnies();
     }
-    if (
-      this.state === "hungry" &&
-      board.foodPositions.length > 0
-    ) {
+    if (this.state === "hungry" && board.foodPositions.length > 0) {
       this.pathForFood();
     }
     if (this.state == "dead") {
@@ -202,25 +197,11 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
           foxId++;
           if (dir === 1) {
             foxArray.push(
-              new Animal(
-                "fox",
-                this.col + 2,
-                this.row + 1,
-                foxId,
-                20,
-                "left"
-              )
+              new Animal("fox", this.col + 2, this.row + 1, foxId, 20, "left")
             );
           } else {
             foxArray.push(
-              new Animal(
-                "fox",
-                this.col + 2,
-                this.row + 1,
-                foxId,
-                20,
-                "right"
-              )
+              new Animal("fox", this.col + 2, this.row + 1, foxId, 20, "right")
             );
           }
           this.state = "idle";
@@ -324,7 +305,7 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
     if (this.animalType === "rabbit") {
       bunniesArray = bunniesArray.filter(bunny => bunny.id != this.id);
       // console.log("another one bites the dust")
-      // console.log(bunniesArray.length) 
+      // console.log(bunniesArray.length)
     }
     // for (let i = 0; i < bunniesArray.length; i++) {
     //   if (this.id === bunniesArray[i].id) {
@@ -348,7 +329,7 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
         this.preyEaten++;
 
         bunniesArray[i].state = "dead";
-      
+
         bunniesArray = bunniesArray.filter(
           bunny => bunny.id !== bunniesArray[i].id
         );
@@ -572,12 +553,10 @@ let Animal = function(animalType, x, y, id, speedModifier, direction) {
           fdbStarvation++;
           writeData("fox", "starvation");
         }
-   
 
         this.moveCounter = 0;
       }
     } else if (this.animalType === "bear") {
-     
       this.moveCounter += 10;
       if (this.pathLength < 10) {
         if (this.currentDirection === "left" && this.col > 0) {
@@ -745,7 +724,7 @@ function writeData(animal, deathby) {
     //fox death logic to db
   } else if (animal === "bear") {
     if (deathby === "starvation") {
-      database.ref("bears/deaths/starvation").set()
+      database.ref("bears/deaths/starvation").set();
     }
   }
 }
@@ -784,7 +763,6 @@ canvas.addEventListener(
   "click",
   function(evt) {
     var mousePos = getMousePos(canvas, evt);
-   
 
     if (toolBox === "grass1") {
       mapArray[mousePos.y][mousePos.x] = GRASS_1;
@@ -825,57 +803,19 @@ function getMousePos(canvas, evt) {
   };
 }
 let bunniesArray = [
-  new Animal(
-    "rabbit",
-    randomNumber(30, 40),
-    randomNumber(5, 10),
-    1,
-    20
-  ),
-  new Animal(
-    "rabbit",
-    randomNumber(30, 40),
-    randomNumber(5, 10),
-    2,
-    20
-  ),
-  new Animal(
-    "rabbit",
-    randomNumber(30, 40),
-    randomNumber(5, 10),
-    3,
-    20
-  ),
-  new Animal(
-    "rabbit",
-    randomNumber(30, 40),
-    randomNumber(5, 10),
-    4,
-    20
-  ),
-  new Animal(
-    "rabbit",
-    randomNumber(30, 40),
-    randomNumber(5, 10),
-    5,
-    20
-  )
-  
+  new Animal("rabbit", randomNumber(30, 40), randomNumber(5, 10), 1, 20),
+  new Animal("rabbit", randomNumber(30, 40), randomNumber(5, 10), 2, 20),
+  new Animal("rabbit", randomNumber(30, 40), randomNumber(5, 10), 3, 20),
+  new Animal("rabbit", randomNumber(30, 40), randomNumber(5, 10), 4, 20),
+  new Animal("rabbit", randomNumber(30, 40), randomNumber(5, 10), 5, 20)
 ];
 let foxArray = [
- 
- 
+  new Animal("fox", randomNumber(30, 40), randomNumber(5, 10), 4, 20, "left"),
+  new Animal("fox", randomNumber(30, 40), randomNumber(5, 10), 5, 20, "right")
 ];
 
 let bearArray = [
-  new Animal(
-    "bear",
-    randomNumber(25, 25),
-    randomNumber(25, 25),
-    50,
-    20,
-    "left"
-  )
+  new Animal("bear", randomNumber(25, 25), randomNumber(25, 25), 50, 20, "left")
 ];
 
 function initialize(animal) {
@@ -918,50 +858,53 @@ $("#playBtn").on("click", () => {
     renderBackground();
 
     $(".gameWrapper").fadeIn("slow", () => {
-      
       startGame();
-    })
-  
+    });
   });
-})
+});
 $("#chaos").on("click", () => {
   maxBunnies = 5000;
-})
+});
 $("#endSim").on("click", () => {
   $(".gameWrapper").fadeOut("slow", () => {
-    $("#dashboard").fadeIn("slow")
-  })
-})
+    stop = true;
+    
+    dashboard("#dashboard", freqData);
+    $("#dashboard").fadeIn("slow");
+  });
+});
+
 // let interval = setInterval(mainLoop, sliderVal / speedModifier);
 
 function startGame() {
-  window.requestAnimationFrame(mainLoop)
+  window.requestAnimationFrame(mainLoop);
 }
 
 function updateSlider() {
-
   let range = $("#range").val();
   sliderVal = range;
 }
 
-function pushTime() {
-  time += 6;
-  timeStampNum++;
-  database.ref(`/timestamps/${timeStampNum}`).set(time);
+function newD3() {
+  if (d3Index < 11) {
+    d3Index++;
+    freqData[d3Index].freq.rabbit = bunniesArray.length;
+    freqData[d3Index].freq.fox = foxArray.length;
+    freqData[d3Index].freq.bear = bearArray.length;
+  }
 }
-setInterval(pushTime, 360000);
-
+setInterval(newD3, 60000);
 
 frameCount = 0;
 function mainLoop() {
-  frameCount+= 1;
+  frameCount += 1;
   if (frameCount < sliderVal) {
     window.requestAnimationFrame(mainLoop);
-    return
+    return;
   }
   frameCount = 0;
   console.log(bunniesArray);
- 
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   initialize();
   updateSlider();
@@ -971,5 +914,381 @@ function mainLoop() {
   if (board.season === 12000) {
     renderBackground();
   }
-  window.requestAnimationFrame(mainLoop)
+  if(!stop) {
+    window.requestAnimationFrame(mainLoop);
+  }
+
 }
+
+function dashboard(id, fData) {
+  var barColor = "steelblue";
+  function segColor(c) {
+    return { bear: "#8b6f5c", fox: "#ffaa00", rabbit: "#faa37d" }[c];
+  }
+
+  // compute total for each animal.
+  fData.forEach(function(d) {
+    d.total = d.freq.bear + d.freq.fox + d.freq.rabbit;
+  });
+
+  // function to handle histogram.
+  function histoGram(fD) {
+    var hG = {},
+      hGDim = { t: 60, r: 0, b: 30, l: 0 };
+    (hGDim.w = 500 - hGDim.l - hGDim.r), (hGDim.h = 300 - hGDim.t - hGDim.b);
+
+    //create svg for histogram.
+    var hGsvg = d3
+      .select(id)
+      .append("svg")
+      .attr("width", hGDim.w + hGDim.l + hGDim.r)
+      .attr("height", hGDim.h + hGDim.t + hGDim.b)
+      .append("g")
+      .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
+
+    // create function for x-axis mapping.
+    var x = d3.scale
+      .ordinal()
+      .rangeRoundBands([0, hGDim.w], 0.1)
+      .domain(
+        fD.map(function(d) {
+          return d[0];
+        })
+      );
+
+    // Add x-axis to the histogram svg.
+    hGsvg
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + hGDim.h + ")")
+      .call(
+        d3.svg
+          .axis()
+          .scale(x)
+          .orient("bottom")
+      );
+
+    // Create function for y-axis map.
+    var y = d3.scale
+      .linear()
+      .range([hGDim.h, 0])
+      .domain([
+        0,
+        d3.max(fD, function(d) {
+          return d[1];
+        })
+      ]);
+
+    // Create bars for histogram to contain rectangles and freq labels.
+    var bars = hGsvg
+      .selectAll(".bar")
+      .data(fD)
+      .enter()
+      .append("g")
+      .attr("class", "bar");
+
+    //create the rectangles.
+    bars
+      .append("rect")
+      .attr("x", function(d) {
+        return x(d[0]);
+      })
+      .attr("y", function(d) {
+        return y(d[1]);
+      })
+      .attr("width", x.rangeBand())
+      .attr("height", function(d) {
+        return hGDim.h - y(d[1]);
+      })
+      .attr("fill", barColor)
+      .on("mouseover", mouseover) // mouseover is defined below.
+      .on("mouseout", mouseout); // mouseout is defined below.
+
+    //Create the frequency labels above the rectangles.
+    bars
+      .append("text")
+      .text(function(d) {
+        return d3.format(",")(d[1]);
+      })
+      .attr("x", function(d) {
+        return x(d[0]) + x.rangeBand() / 2;
+      })
+      .attr("y", function(d) {
+        return y(d[1]) - 5;
+      })
+      .attr("text-anchor", "middle");
+
+    function mouseover(d) {
+      // utility function to be called on mouseover.
+      // filter for selected state.
+      var st = fData.filter(function(s) {
+          return s.State == d[0];
+        })[0],
+        nD = d3.keys(st.freq).map(function(s) {
+          return { type: s, freq: st.freq[s] };
+        });
+
+      // call update functions of pie-chart and legend.
+      pC.update(nD);
+      leg.update(nD);
+    }
+
+    function mouseout(d) {
+      // utility function to be called on mouseout.
+      // reset the pie-chart and legend.
+      pC.update(tF);
+      leg.update(tF);
+    }
+
+    // create function to update the bars. This will be used by pie-chart.
+    hG.update = function(nD, color) {
+      // update the domain of the y-axis map to reflect change in frequencies.
+      y.domain([
+        0,
+        d3.max(nD, function(d) {
+          return d[1];
+        })
+      ]);
+
+      // Attach the new data to the bars.
+      var bars = hGsvg.selectAll(".bar").data(nD);
+
+      // transition the height and color of rectangles.
+      bars
+        .select("rect")
+        .transition()
+        .duration(500)
+        .attr("y", function(d) {
+          return y(d[1]);
+        })
+        .attr("height", function(d) {
+          return hGDim.h - y(d[1]);
+        })
+        .attr("fill", color);
+
+      // transition the frequency labels location and change value.
+      bars
+        .select("text")
+        .transition()
+        .duration(500)
+        .text(function(d) {
+          return d3.format(",")(d[1]);
+        })
+        .attr("y", function(d) {
+          return y(d[1]) - 5;
+        });
+    };
+    return hG;
+  }
+
+  // function to handle pieChart.
+  function pieChart(pD) {
+    var pC = {},
+      pieDim = { w: 250, h: 250 };
+    pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+
+    // create svg for pie chart.
+    var piesvg = d3
+      .select(id)
+      .append("svg")
+      .attr("width", pieDim.w)
+      .attr("height", pieDim.h)
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + pieDim.w / 2 + "," + pieDim.h / 2 + ")"
+      );
+
+    // create function to draw the arcs of the pie slices.
+    var arc = d3.svg
+      .arc()
+      .outerRadius(pieDim.r - 10)
+      .innerRadius(0);
+
+    // create a function to compute the pie slice angles.
+    var pie = d3.layout
+      .pie()
+      .sort(null)
+      .value(function(d) {
+        return d.freq;
+      });
+
+    // Draw the pie slices.
+    piesvg
+      .selectAll("path")
+      .data(pie(pD))
+      .enter()
+      .append("path")
+      .attr("d", arc)
+      .each(function(d) {
+        this._current = d;
+      })
+      .style("fill", function(d) {
+        return segColor(d.data.type);
+      })
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout);
+
+    // create function to update pie-chart. This will be used by histogram.
+    pC.update = function(nD) {
+      piesvg
+        .selectAll("path")
+        .data(pie(nD))
+        .transition()
+        .duration(500)
+        .attrTween("d", arcTween);
+    };
+    // Utility function to be called on mouseover a pie slice.
+    function mouseover(d) {
+      // call the update function of histogram with new data.
+      hG.update(
+        fData.map(function(v) {
+          return [v.State, v.freq[d.data.type]];
+        }),
+        segColor(d.data.type)
+      );
+    }
+    //Utility function to be called on mouseout a pie slice.
+    function mouseout(d) {
+      // call the update function of histogram with all data.
+      hG.update(
+        fData.map(function(v) {
+          return [v.State, v.total];
+        }),
+        barColor
+      );
+    }
+    // Animating the pie-slice requiring a custom function which specifies
+    // how the intermediate paths should be drawn.
+    function arcTween(a) {
+      var i = d3.interpolate(this._current, a);
+      this._current = i(0);
+      return function(t) {
+        return arc(i(t));
+      };
+    }
+    return pC;
+  }
+
+  // function to handle legend.
+  function legend(lD) {
+    var leg = {};
+
+    // create table for legend.
+    var legend = d3
+      .select(id)
+      .append("table")
+      .attr("class", "legend");
+
+    // create one row per segment.
+    var tr = legend
+      .append("tbody")
+      .selectAll("tr")
+      .data(lD)
+      .enter()
+      .append("tr");
+
+    // create the first column for each segment.
+    tr.append("td")
+      .append("svg")
+      .attr("width", "16")
+      .attr("height", "16")
+      .append("rect")
+      .attr("width", "16")
+      .attr("height", "16")
+      .attr("fill", function(d) {
+        return segColor(d.type);
+      });
+
+    // create the second column for each segment.
+    tr.append("td").text(function(d) {
+      return d.type;
+    });
+
+    // create the third column for each segment.
+    tr.append("td")
+      .attr("class", "legendFreq")
+      .text(function(d) {
+        return d3.format(",")(d.freq);
+      });
+
+    // create the fourth column for each segment.
+    tr.append("td")
+      .attr("class", "legendPerc")
+      .text(function(d) {
+        return getLegend(d, lD);
+      });
+
+    // Utility function to be used to update the legend.
+    leg.update = function(nD) {
+      // update the data attached to the row elements.
+      var l = legend
+        .select("tbody")
+        .selectAll("tr")
+        .data(nD);
+
+      // update the frequencies.
+      l.select(".legendFreq").text(function(d) {
+        return d3.format(",")(d.freq);
+      });
+
+      // update the percentage column.
+      l.select(".legendPerc").text(function(d) {
+        return getLegend(d, nD);
+      });
+    };
+
+    function getLegend(d, aD) {
+      // Utility function to compute percentage.
+      return d3.format("%")(
+        d.freq /
+          d3.sum(
+            aD.map(function(v) {
+              return v.freq;
+            })
+          )
+      );
+    }
+
+    return leg;
+  }
+
+  // calculate total frequency by segment for all state.
+  var tF = ["bear", "fox", "rabbit"].map(function(d) {
+    return {
+      type: d,
+      freq: d3.sum(
+        fData.map(function(t) {
+          return t.freq[d];
+        })
+      )
+    };
+  });
+
+  // calculate total frequency by state for all segment.
+  var sF = fData.map(function(d) {
+    return [d.State, d.total];
+  });
+
+  var hG = histoGram(sF), // create the histogram.
+    pC = pieChart(tF), // create the pie-chart.
+    leg = legend(tF); // create the legend.
+}
+
+var freqData = [
+  { State: "00:06", freq: { bear: 5, fox: 5, rabbit: 5 } },
+  { State: "00:12", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:18", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:24", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:30", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:36", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:42", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:48", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "00:54", freq: { bear: 0, fox: 0, rabbit: 0 } },
+  { State: "01:00", freq: { bear: 0, fox: 0, rabbit: 0 } }
+];
+freqData[d3Index].freq.rabbit = bunniesArray.length;
+freqData[d3Index].freq.fox = foxArray.length;
+freqData[d3Index].freq.bear = bearArray.length;
+
+d3.selectAll("text").attr("fill", "white");
